064b2e762235648c3c5d612b134cdde030891197
Shashank Kumar Shukla <skshukl01@gmail.com>
2022-02-14 15:06:58 +0530

    CORS policy updated successfully v2

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(bodyParser.urlencoded({
 }))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next()
 })
 
